a1312be9cb33abd431a11a8213a393dabff0293b
ChaneLaForet <72274437+ChaneLaForet@users.noreply.github.com>
2021-06-26 13:13:14 +0300

    Update Login test.

--- Test_EndToEnd.cs
+++ Test_EndToEnd.cs
@@ -2,7 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using OpenQA.Selenium.Support.UI;
 
 namespace TestsLab6
 {
@@ -22,43 +22,32 @@ namespace TestsLab6
             _driver.Close();
         }
 
-        /*
-        [Test]
-        public void EmagGeniusTextExists()
-        {
-            _driver.Url = "https://www.emag.ro";
-            bool foundGenius = false;
-            foreach (var span in _driver.FindElements(By.TagName("span")))
-            {
-                if (span.Text == "eMAG Genius")
-                {
-                    foundGenius = true;
-                    break;
-                }
-            }
-            Assert.IsTrue(foundGenius);
-        }
-        */
-
         [Test]
         public void LoginIsFunctional()
         {
             _driver.Url = "http://localhost:4200/login";
             bool isLoggedIn = false;
+
+            wait(2000);
+
             try
             {
+
                 //var email = _driver.FindElement(By.XPath("//*[@id='content1']/app-login/ion-content/div/form/ion-item[1]/ion-input"));
                 //var email = _driver.FindElement(By.Name("email"));
-                var email = _driver.FindElement(By.TagName("ion-input"));
+                var email = _driver.FindElement(By.Name("email")).FindElement(By.Name("email"));
                 email.Click();
                 email.SendKeys("mary.sue@gmail.com");
-                
-                var password = _driver.FindElement(By.Name("password"));
+                wait(1000);
+
+                var password = _driver.FindElement(By.Name("password")).FindElement(By.Name("password")); ;
                 password.Click();
                 password.SendKeys("asdASD1234!@#$");
-                
+                wait(2000);
+
                 var submit = _driver.FindElement(By.TagName("ion-button"));
                 submit.Click();
+                wait(2000);
 
                 var ionTitle = _driver.FindElement(By.XPath("//ion-title"));
                 if (ionTitle.Text == "Movies")
@@ -73,6 +62,49 @@ namespace TestsLab6
                 Assert.Fail("User is not logged in.");
             }
         }
+
+        /* //element is not interactable
+        [Test]
+        public void LoginIsFunctional()
+        {
+            _driver.Url = "http://localhost:4200/login";
+            bool isLoggedIn = false;
+
+            wait(2000);
+
+            try
+            {
+
+                //var email = _driver.FindElement(By.XPath("//*[@id='content1']/app-login/ion-content/div/form/ion-item[1]/ion-input"));
+                //var email = _driver.FindElement(By.Name("email"));
+                 _driver.FindElement(By.TagName("ion-input")).SendKeys("aaa");
+                email.Click();
+                email.SendKeys("mary.sue@gmail.com");
+                wait(1000);
+                
+                var password = _driver.FindElement(By.Name("password"));
+                password.Click();
+                password.SendKeys("asdASD1234!@#$");
+                wait(1000);
+                
+                var submit = _driver.FindElement(By.TagName("ion-button"));
+                submit.Click();
+                wait(2000);
+
+                var ionTitle = _driver.FindElement(By.XPath("//ion-title"));
+                if (ionTitle.Text == "Movies")
+                {
+                    isLoggedIn = true;
+                }
+
+                Assert.IsTrue(isLoggedIn);
+            }
+            catch (NoSuchElementException)
+            {
+                Assert.Fail("User is not logged in.");
+            }
+        }
+        */
 
         /*
         [Test]
@@ -120,6 +152,20 @@ namespace TestsLab6
 
             var submit = _driver.FindElement(By.TagName("ion-button"));
             submit.Click();
+        }
+
+        private void wait(int timeout)
+        {
+            //var timeout = 5000;
+            var wait = new WebDriverWait(_driver, TimeSpan.FromMilliseconds(timeout));
+            try
+            {
+                wait.Until(d => false);
+            }
+            catch (WebDriverTimeoutException wdtex)
+            {
+
+            }
         }
     }
 }
